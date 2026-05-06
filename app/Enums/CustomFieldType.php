58187118d928ecc34892a9fd9fa2e917c7c6b552
Suliman <salmanyousef13@gmail.com>
2026-05-06 21:29:48 +0300

    some update

--- app/Enums/CustomFieldType.php
+++ app/Enums/CustomFieldType.php
@@ -5,7 +5,9 @@ namespace App\Enums;
 enum CustomFieldType: string
 {
     case Text = 'text'; // text custom field
-    case Textarea = 'textarea';
+    case Textarea = 'textarea'; // textarea custom field
+    case Email = 'email'; // email custom field
+    case Phone = 'phone'; // phone custom field
     case Number = 'number';
     case Date = 'date';
     case Select = 'select';
