69eb1bd397d5eb5e0c75cdb61c2e45641e5a2f93
Suliman <salmanyousef13@gmail.com>
2026-05-06 21:28:38 +0300

    some update

--- app/Enums/ComplaintStatus.php
+++ app/Enums/ComplaintStatus.php
@@ -6,7 +6,7 @@ enum ComplaintStatus: string
 {
     case New = 'new'; // new complaint
     case UnderReview = 'under_review'; // complaint under review
-    case WaitingCustomer = 'waiting_customer';
+    case WaitingCustomer = 'waiting_customer'; // waiting for customer response
     case Escalated = 'escalated';
     case Resolved = 'resolved';
     case Closed = 'closed';
