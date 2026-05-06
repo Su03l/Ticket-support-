246ab39dcbf18a347822300e654d963fe1163028
Suliman <salmanyousef13@gmail.com>
2026-05-06 21:29:23 +0300

    some update

--- app/Enums/CustomFieldAppliesTo.php
+++ app/Enums/CustomFieldAppliesTo.php
@@ -4,8 +4,8 @@ namespace App\Enums;
 
 enum CustomFieldAppliesTo: string
 {
-    case Ticket = 'ticket';
-    case Complaint = 'complaint';
-    case Inquiry = 'inquiry';
-    case Customer = 'customer';
+    case Ticket = 'ticket'; // custom fields for ticket
+    case Complaint = 'complaint'; // custom fields for complaint
+    case Inquiry = 'inquiry'; // custom fields for inquiry
+    case Customer = 'customer'; // custom fields for customer
 }
