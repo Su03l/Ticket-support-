ff3a5b1abd9112dfd7cdf4578d9a54960ece0b9f
Suliman <salmanyousef13@gmail.com>
2026-05-07 09:33:46 +0300

    some update

--- app/Enums/SlaAppliesTo.php
+++ app/Enums/SlaAppliesTo.php
@@ -5,6 +5,6 @@ namespace App\Enums;
 enum SlaAppliesTo: string
 {
     case Tickets = 'tickets'; // tickets sla applies to
-    case Complaints = 'complaints';
-    case Inquiries = 'inquiries';
+    case Complaints = 'complaints'; // complaints sla applies to
+    case Inquiries = 'inquiries'; // inquiries sla applies to
 }
