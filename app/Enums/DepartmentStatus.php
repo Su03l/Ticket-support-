84489f588a06ff4d2d23511fd7c06504349010f9
Suliman <salmanyousef13@gmail.com>
2026-05-07 09:26:31 +0300

    some update

--- app/Enums/DepartmentStatus.php
+++ app/Enums/DepartmentStatus.php
@@ -6,5 +6,5 @@ enum DepartmentStatus: string
 {
     case Active = 'active'; // active department status
     case Inactive = 'inactive'; // inactive department status
-    case Archived = 'archived';
+    case Archived = 'archived'; // archived department status
 }
